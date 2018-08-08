ea1d0de3f2dca51ca0b04f46195c1bbacfd2a598
lv-conner <lvmh123456@gmail.com>
2018-08-08 17:36:36 +0800

    添加读去基础类库

---
+++ UnityAuthorizatioin.Query/Interface/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UnityAuthorizatioin.Query.Interface
+{
+    public interface IQuery<T> where T:class
+    {
+        T Get(string key);
+    }
+}
---
+++ UnityAuthorizatioin.Query/QueryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityAuthorizatioin.Query.Interface;
+
+namespace UnityAuthorizatioin.Query
+{
+    public abstract class QueryBase<T> : IQuery<T> where T:class
+    {
+        public T Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
